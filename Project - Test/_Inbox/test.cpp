09e302581a38e8bab0c884428137a79d38b8f202
Takshkanzariya930 <takshkanzariya930@gmail.com>
2025-02-01 22:01:08 +0530

    Testing C++

--- Project - Test/_Inbox/test.cpp
+++ Project - Test/_Inbox/test.cpp
@@ -1,9 +1,12 @@
-#include<iostream>
-using namespace std;
-
+#include <graphics.h>
+#include <iostream>
 int main()
 {
-	int a;
-	cin>>a;
-	cout<<"Hello World!!"<<a;
+int gd = DETECT, gm;
+initgraph(&gd, &gm, (char*)"");
+line(200, 100, 100, 200);
+line(200, 100, 300, 200);
+line(100, 200, 300, 200);
+getch();
+closegraph();
 }
